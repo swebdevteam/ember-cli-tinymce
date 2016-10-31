d3552646dd5449cc26dd24e6ca8bf3265f9172b2
Rudy Macias <rudymacias@me.com>
2016-10-31 16:10:23 -0500

    Multiple editors update
    Adding ability to edit multiple editors.

--- addon/components/tinymce-editor.js
+++ addon/components/tinymce-editor.js
@@ -4,12 +4,24 @@ const {observer, on, run} = Ember;
 export default Ember.Component.extend({
   editor: undefined,
   tagName: 'textarea',
-
+  
   // Change the editor content if value changes
   valueChanged: observer('value', function() {
-    let editor = this.get('editor');
-    if (editor && editor.getContent() !== this.get('value')) {
-      editor.setContent(this.get('value') || '');
+    var id = this.get('id');
+    if (id == 'edit') {
+       let editor = this.get('editor');
+      if (editor && editor.getContent() !== this.get('value')) {
+        //console.log(this.get('value'));
+        editor.setContent(this.get('value'));
+      }
+    }
+    if (id == 'edit2') {
+      let editor = this.get('editor');
+      //check and see why editor2 is not able to get data
+      if (editor && editor.getContent() !== this.get('value')) {
+        //console.log('edit 2 ' + this.get('value'));
+        editor.setContent(this.get('value'));
+      }
     }
   }),
 
@@ -27,30 +39,54 @@ export default Ember.Component.extend({
   }),
 
   // Initialize tinymce
-  initTiny: on('didInsertElement', observer('options', function() {
-    let {options, editor} = this.getProperties('options', 'editor');
-
-    let customOptions = {
-      selector: `#${this.get('elementId')}`,
-      init_instance_callback : (editor) => {
-        this.set('editor', editor);
-        this.get('editor').setContent(this.get('value') || ''); //Set content with default text
-      },
-    };
-
-    if (editor){
-      editor.destroy();
+  initTiny: on('didInsertElement', observer('options', 'options2', function() {
+    var id = this.get('id');
+    console.log(id);
+    if (id == 'edit') {
+      let {options, editor} = this.getProperties('options', 'editor');
+      let customOptions = {
+        inline: true,
+        images_upload_base_path: '/some/basepath',
+        automatic_uploads: false,
+        images_dataimg_filter: function(img) {
+          return img.hasAttribute('internal-blob');
+        },
+        images_upload_credentials: true,
+        selector: '.editable', //selector: `#${this.get('elementId')}`,
+        plugins: [
+          "advlist autolink lists link image charmap print preview anchor",
+          "searchreplace visualblocks code fullscreen",
+          "insertdatetime media table contextmenu paste"
+        ],
+        init_instance_callback : (editor) => {
+          this.set('editor', editor);
+          this.get('editor').setContent(this.get('value') || ''); //Set content with default text
+        },
+      };
+      tinymce.init(Ember.$.extend( customOptions, options ));
+    } else {
+      let {options, editor} = this.getProperties('options', 'editor');
+      let customOptions = {
+        inline: true,
+        images_upload_base_path: '/some/basepath',
+        automatic_uploads: false,
+        images_dataimg_filter: function(img) {
+          return img.hasAttribute('internal-blob');
+        },
+        images_upload_credentials: true,
+        selector: '.editable2', //selector: `#${this.get('elementId')}`,
+        plugins: [
+          "advlist autolink lists link image charmap print preview anchor",
+          "searchreplace visualblocks code fullscreen",
+          "insertdatetime media table contextmenu paste"
+        ],
+        init_instance_callback : (editor) => {
+          this.set('editor', editor);
+          this.get('editor').setContent(this.get('value') || ''); //Set content with default text
+        },
+      };
+      tinymce.init(Ember.$.extend( customOptions, options ));
     }
-
-    tinymce.init(Ember.$.extend( customOptions, options ));
+    
   })),
-
-  // Destroy tinymce editor instance when editor is removed from the page.  Otherwise, it won't be
-  // created again when added back to the page (i.e. navigating away from and back to the route).
-  cleanUp: on('willDestroyElement', function() {
-    let editor = this.get('editor');
-    if (editor) {
-      editor.destroy();
-    }
-  })
 });
